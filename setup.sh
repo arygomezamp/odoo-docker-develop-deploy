02f7509dd50499baf4d36c80ffbec5e2aaedf7dd
aryamp <arygomez@ampsoftware.com>
2025-03-04 12:18:31 +0100

    FIX odoo.conf

--- setup.sh
+++ setup.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-# Definir las rutas del proyecto y las rutas fuera del proyecto para los módulos
+# Definir las rutas del proyecto y las rutas fuera del proyecto para los módulos y los datos de PostgreSQL
 PROJECT_DIR="/opt/odoo/odoo-docker-develop-deploy"
 OCA_FILE="$PROJECT_DIR/OCA.txt"
 CUSTOM_FILE="$PROJECT_DIR/custom.txt"
 OCA_DIR="/opt/odoo/oca_modules"  # Fuera de la estructura del proyecto
 ADDONS_DIR="/opt/odoo/custom_modules"  # Fuera de la estructura del proyecto
+POSTGRESQL_DATA_DIR="/opt/odoo/postgresql-data"  # Fuera de la estructura del proyecto
 ODOO_CONF="$PROJECT_DIR/config/odoo.conf"
 
 # Verificar si Docker está instalado
@@ -23,7 +24,7 @@ if ! command -v docker-compose &> /dev/null; then
 fi
 
 # Crear directorios necesarios fuera del proyecto
-mkdir -p "$ADDONS_DIR" "$OCA_DIR" "$PROJECT_DIR/config" "$PROJECT_DIR/postgresql-data"
+mkdir -p "$ADDONS_DIR" "$OCA_DIR" "$POSTGRESQL_DATA_DIR" "$PROJECT_DIR/config"
 
 # Descargar y actualizar repositorios OCA fuera del proyecto
 echo "Descargando repositorios OCA..."
@@ -53,23 +54,29 @@ while read -r repo_url; do
     fi
 done < "$CUSTOM_FILE"
 
-# Generar odoo.conf con las rutas de los addons (fuera de la estructura del proyecto)
-echo "Generando odoo.conf..."
-ADDONS_PATH=$(find "$ADDONS_DIR" -mindepth 1 -maxdepth 1 -type d | paste -sd "," -)
-OCA_PATH=$(find "$OCA_DIR" -mindepth 1 -maxdepth 1 -type d | paste -sd "," -)
-ADDONS_ALL_PATH="$OCA_PATH,$ADDONS_PATH"  # Combina las rutas de OCA y Custom
+# Verificar si el archivo de configuración ya existe
+if [ -f "$ODOO_CONF" ]; then
+    echo "Archivo de configuración odoo.conf encontrado en $ODOO_CONF."
+else
+    # Generar odoo.conf si no existe
+    echo "Generando odoo.conf..."
+    ADDONS_PATH=$(find "$ADDONS_DIR" -mindepth 1 -maxdepth 1 -type d | paste -sd "," -)
+    OCA_PATH=$(find "$OCA_DIR" -mindepth 1 -maxdepth 1 -type d | paste -sd "," -)
+    ADDONS_ALL_PATH="$OCA_PATH,$ADDONS_PATH"  # Combina las rutas de OCA y Custom
 
-# Escribir el archivo de configuración
-cat > "$ODOO_CONF" <<EOF
+    # Escribir el archivo de configuración
+    cat > "$ODOO_CONF" <<EOF
 [options]
 addons_path = $ADDONS_ALL_PATH
-db_host = localhost
+db_host = db
 db_port = 5432
 db_user = odoo
 db_password = odoo
 admin_passwd = admin
 log_level = debug
 EOF
+    echo "Archivo odoo.conf generado en $ODOO_CONF."
+fi
 
 # Levantar los servicios de Docker
 echo "Levantando entorno Odoo..."
